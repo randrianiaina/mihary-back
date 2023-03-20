6d249db2a18f4dc2cdc9267a77f7ee1042eab6cc
Nomenjanahary Herizo RANDRIANIAINA <menja.herizo@gmail.com>
2023-03-20 17:20:18 +0300

    mandamina fichier

--- README.md
+++
@@ -1,1 +0,0 @@
-# mihary-back
--- src/main/java/mg/inclusiv/mihary/repository/ApprovisionnementRepository.java
+++
@@ -1,7 +0,0 @@
-package mg.inclusiv.mihary.repository;
-
-import mg.inclusiv.mihary.entity.Approvisionnement;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface ApprovisionnementRepository extends JpaRepository<Approvisionnement, Long> {
-}
